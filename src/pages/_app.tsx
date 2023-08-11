d5efde1a24457fba4672dfbadd3e245fa63ace1e
Siddharth Brahmecha <siddharth.brahmecha@trainingmug.com>
2023-08-11 16:15:04 +0530

    feat: oauth added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
+import { Session } from 'next-auth';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type Props = {
+  session:Session | null;
+}
+
+export default function App({ Component, pageProps }: AppProps,{session}:Props) {
+  return <SessionProvider session={session}>
+    <Component {...pageProps} />
+  </SessionProvider>
 }
