638899abef5e8381916ce4edeb5846ff9ee536f3
fadkeabhi <31249309+fadkeabhi@users.noreply.github.com>
2023-08-14 23:12:26 +0530

    creating functions for graphbase integration

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -7,17 +7,25 @@ const message = g.model('Message', {
 
 const chat = g.model('Chat', {
   chatName: g.string(),
-  conversations: g.relation(message).optional().list()
+  conversations: g.relation(message).optional().list().optional()
 })
 
 const user = g.model('User',{
-  email: g.string(),
-  chats: g.relation(chat).optional().list()
+  email: g.string().unique().search(),
+  chats: g.relation(chat).optional().list().optional()
 })
 
 
 export default config({
-  schema: g
+  schema: g,
+
+  auth: {
+    rules: rules => {
+      rules.public()
+    }
+  }
+
+
   // Integrate Auth
   // https://grafbase.com/docs/auth
   // auth: {
